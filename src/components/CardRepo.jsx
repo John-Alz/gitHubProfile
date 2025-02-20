9fa04f6253e1820e77b147b3c1bbf5db039da824
John-Alz <99442406+John-Alz@users.noreply.github.com>
2025-02-20 18:17:33 -0500

    finish

--- src/components/CardRepo.jsx
+++ src/components/CardRepo.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export const CardRepo = ({ repo }) => {
+
     return (
         <div className=' bg-linear-to-r from-cardP to-tertiary flex flex-col gap-3 p-4 rounded-2xl'>
             <h2 className='text-lg'>{repo.name}</h2>
             <p className='text-sm'>{repo.description}</p>
-            <div className='flex flex-wrap gap-3'>
+            <div className='flex flex-wrap gap-3 items-center'>
                 <span className='flex'><img src={'/Nesting.svg'} alt='repoFork' />{repo.forks_count}</span>
                 <span className='flex items-center'><img src={'/Star.svg'} alt='repoStar' />{repo.stargazers_count}</span>
-                <span>{repo.updated_at}</span>
+                <span className='text-xs'>Updated {repo.updated_at.slice(0, 10)}</span>
             </div>
         </div>
     )
